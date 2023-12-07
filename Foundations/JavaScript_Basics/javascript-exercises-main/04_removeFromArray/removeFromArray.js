7559e8d91ce33bf17c6ac9d002c000770e259ac3
brandon.trainee <brandon.trainee@gmail.com>
2023-12-07 16:19:44 +1000

    Completed JS exercise 4.

--- Foundations/JavaScript_Basics/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ Foundations/JavaScript_Basics/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...exclude) {
+    const newArray = [];
+    input.forEach((item) => {
+        if (!exclude.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
