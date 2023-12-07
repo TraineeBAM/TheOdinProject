4fe907847bd8cc1ac282bf7ea39190c785f9f762
brandon.trainee <brandon.trainee@gmail.com>
2023-12-07 15:32:22 +1000

    Completed JS exercise #3.

--- Foundations/JavaScript_Basics/javascript-exercises-main/03_reverseString/reverseString.js
+++ Foundations/JavaScript_Basics/javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let reversedString = "";
+    const count = input.length;
 
-};
+    for (let i = count - 1; i >= 0; i--){
+        reversedString += input[i];
+    }
+
+    return reversedString;
+}
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
