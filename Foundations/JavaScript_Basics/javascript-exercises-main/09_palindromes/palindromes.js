fa3070e2bb8b8f2c28eb265eed503076d1afd5a7
brandon.trainee <brandon.trainee@gmail.com>
2023-12-08 16:00:23 +1000

    Completed JS exercise #9

--- Foundations/JavaScript_Basics/javascript-exercises-main/09_palindromes/palindromes.js
+++ Foundations/JavaScript_Basics/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function(input) {
+    const noSpecialChars = input.replace(/[^a-zA-Z0-9]/g, "");
+    const lowerCase = noSpecialChars.toLowerCase();
+    const reversedString = lowerCase.split("").reverse().join("");
+    return lowerCase === reversedString;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
