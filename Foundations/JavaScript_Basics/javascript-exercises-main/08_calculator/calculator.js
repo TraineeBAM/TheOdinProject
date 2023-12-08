2be862995d62ff4584265eccb061849b804e94dc
brandon.trainee <brandon.trainee@gmail.com>
2023-12-08 14:55:41 +1000

    Completed JS Exercise #8

--- Foundations/JavaScript_Basics/javascript-exercises-main/08_calculator/calculator.js
+++ Foundations/JavaScript_Basics/javascript-exercises-main/08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let total = 0;
+    array.forEach(item => {
+      total += item;
+    });
+    return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    let total = 1;
+    array.forEach(item => {
+      total *= item;
+    });
+    return total;
+}
 
+const power = function(a, b) {
+    return a ** b
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(x) {
+  let sum = 1;
+  for(let i = 1; i <= x; i++){
+      sum *= i;
+  }
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = {
