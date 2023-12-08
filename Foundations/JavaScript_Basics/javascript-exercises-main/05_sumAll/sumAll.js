347e56056d3f925bb9a22f0f2c4144a35dbe99e9
brandon.trainee <brandon.trainee@gmail.com>
2023-12-08 10:41:41 +1000

    Completed JS exercise 5

--- Foundations/JavaScript_Basics/javascript-exercises-main/05_sumAll/sumAll.js
+++ Foundations/JavaScript_Basics/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b <0) return "ERROR";
+    if(a > b) {([a,b] = [b,a])};
+    if(Number. isInteger(a) != true) return "ERROR";
+    if(Number. isInteger(b) != true) return "ERROR";
+    let newSum = [];
+    for(i = a ; i <= b; i++){
+        newSum.push(i);
+    }
+    let sum = 0;
+    for (let i = 0; i < newSum.length; i++){
+        sum += newSum[i];
+    }
+    return sum;
+}
 
-};
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
