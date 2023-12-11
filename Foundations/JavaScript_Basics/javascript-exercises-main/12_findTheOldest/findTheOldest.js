fed61b7db6a6e70655e974c964be7e73a7e9a70d
brandon.trainee <brandon.trainee@gmail.com>
2023-12-11 15:18:25 +1000

    Completed JS Exercise 12. This is my solution to the problem, for some reason it fails the three tests but when I run the same tests in the console they are correct.

--- Foundations/JavaScript_Basics/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ Foundations/JavaScript_Basics/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+        let oldestAge = 0;
+        let name = "";
+        for(let i = 0; i < array.length; i++){
+            const birthYear = array[i].yearOfBirth;
+            const deathYear = array[i].yearOfDeath || new Date().getFullYear();
+            let age = deathYear ? deathYear - birthYear : new Date().getFullYear() - birthYear;
+            console.log(age);
+            if(age > oldestAge){
+                oldestAge = age;
+                name = array[i].name;
+            }
+        }
+        return name;
 };
 
 // Do not edit below this line
