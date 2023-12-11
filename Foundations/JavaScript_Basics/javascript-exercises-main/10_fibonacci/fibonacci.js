ab2d46d8522a927ee1e15e6ed4fe5920ec9dd64f
brandon.trainee <brandon.trainee@gmail.com>
2023-12-11 13:44:04 +1000

    Finished JS Exercise #10

--- Foundations/JavaScript_Basics/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ Foundations/JavaScript_Basics/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+        Number(x);
+        if(x == 1) return 1;
+        if(x == 2) return 1;
+        if(x < 0) return "OOPS";
+        let old = 1;
+        let current = 1;
+        for (let i = 3; i <= x ; i++) {
+            value = old + current;
+            old = current;
+            current = value;
+        }
+        return value;
 };
 
 // Do not edit below this line
