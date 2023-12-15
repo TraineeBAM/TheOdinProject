477741590ff845d66749d48ecfa6f3280ac6eb16
brandon.trainee <brandon.trainee@gmail.com>
2023-12-15 10:10:36 +1000

    Added ability to genereate grid

--- Foundations/projects/etch-a-sketch/script.js
+++ Foundations/projects/etch-a-sketch/script.js
@@ -13,6 +13,47 @@ title.style.marginBottom = "40px";
 title.style.fontSize = "75px";
 body.appendChild(title);
 
+const regen = document.createElement('button');
+regen.textContent = "Regenerate Grid";
+regen.style.textAlign = "center";
+regen.style.width = "200px";
+regen.style.height = "50px";
+regen.style.fontSize = "20px";
+regen.style.marginBottom = "40px"
+regen.addEventListener('click', function(){
+    let userInput = prompt("Please enter grid size")
+    if(userInput >= 2 && userInput <= 100){
+        generateGrid(userInput);
+    }
+    else {
+        alert("Please enter a value between 2 and 100")
+    }
+});
+body.appendChild(regen);
+
+function generateGrid(size){
+    gridContainer.innerHTML = '';
+
+    for(let i = 0 ; i < size; i ++){
+        const row = document.createElement('div');
+        row.style.display = 'flex';
+        row.style.flexDirection = 'row';
+
+        for(let j = 0 ; j < size ; j ++){
+            const square = createSquare();
+            row.appendChild(square);
+        }
+        gridContainer.appendChild(row);
+    }
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', function(){
+            square.style.transition = 'background-color 0.2s ease-in-out'
+            square.style.backgroundColor = "#77C3EC";
+        });
+    });
+}
+
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('grid-container');
 body.appendChild(gridContainer);
@@ -27,29 +68,15 @@ function createSquare() {
     return square;
 }
 
-// Creating a 16x16 grid
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.style.display = 'flex';
-    row.style.flexDirection = 'row';
-    
-    for (let j = 0; j < 16; j++) {
-        const square = createSquare();
-        row.appendChild(square);
-    }
-
-    gridContainer.appendChild(row);
-}
-
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.addEventListener('mouseover', function(){
-        square.style.transition = 'background-color 0.2s ease-in-out'
-        square.style.backgroundColor = "#77C3EC";
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('mouseover', function(){
+        button.style.transition = "transform 0.2s ease-in-out";
+        button.style.transform = "scale(1.05)";
     });
-
-    //square.addEventListener('mouseout', function(){
-        //square.style.transition = 'background-color 0.5s ease-in-out';
-        //square.style.backgroundColor = '#D3D3D3'
-    //});
+    
+    button.addEventListener('mouseout', function(){
+        button.style.transition = 'transform 0.2s ease-in-out';
+        button.style.transform = 'scale(1)';
+    });
 });
