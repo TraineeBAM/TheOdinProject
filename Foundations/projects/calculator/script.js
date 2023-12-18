c7af87130e88608227477707c340fc5e8e04533b
brandon.trainee <brandon.trainee@gmail.com>
2023-12-18 15:41:34 +1000

    JS screen logic completed

---
+++ Foundations/projects/calculator/script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('mouseover', function(){
+        button.style.transition = "transform 0.2s ease-in-out";
+        button.style.transform = "scale(0.95)";
+    });
+    
+    button.addEventListener('mouseout', function(){
+        button.style.transition = 'transform 0.2s ease-in-out';
+        button.style.transform = 'scale(1)';
+    });
+});
+
+const screen = document.querySelector('.screen');
+const userInput = document.createElement('div');
+userInput.classList.add('userInput')
+userInput.textContent = "";
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        if(button.textContent === "Clear"){
+            userInput.textContent = "";
+        } else if(button.textContent === "Delete"){
+            userInput.textContent = userInput.textContent.slice(0, -1);
+        }
+        
+        else {
+            userInput.textContent += button.textContent;
+        }
+        screen.appendChild(userInput);
+    });
+});
+screen.appendChild(userInput)
