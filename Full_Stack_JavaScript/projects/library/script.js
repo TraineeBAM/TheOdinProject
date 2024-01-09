77cfa1edab39499b2eb46077924f55badc872483
brandon.trainee <brandon.trainee@gmail.com>
2024-01-09 21:04:40 +1000

    Library project: new card functionatlity

--- Full_Stack_JavaScript/projects/library/script.js
+++ Full_Stack_JavaScript/projects/library/script.js
@@ -5,15 +5,40 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.favorite = favorite;
+    this.rating = rating;
 }
 
 // function addBookToLibrary(){
 //         myLibrary.push(`${this.Book}`)
 // };
 
-let demo_book = new Book(
-    'The Hobbit',
-    'J.R.R Tolkein',
-    310,
-    'read');
+// let demo_book = new Book(
+//     'The Hobbit',
+//     'J.R.R Tolkein',
+//     310,
+//     'read');
 
+const form = document.querySelector("#book-form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const name = data.get("title")
+    const entries = Object.fromEntries(data.entries());
+    myLibrary.push(entries);
+    form.reset();
+
+    const content_cards_container = document.getElementById('content_cards_container');
+    const new_card = document.createElement('div');
+    new_card.classList.add('new_card');
+    const { title, author, pages, rating } = entries;
+    new_card.innerHTML = `
+        <h1>${title}</h1>
+        <h2>by ${author}</h2>
+        <h3>${pages} pages</h3>
+        <h3>${rating}</h3>
+    `;
+    content_cards_container.appendChild(new_card);
+
+});
