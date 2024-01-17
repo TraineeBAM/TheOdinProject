f50cd4d98350eb4052c3e664f8e1c65a83ca796f
brandon.trainee <brandon.trainee@gmail.com>
2024-01-17 12:24:25 +1000

    Finalising logic for project

--- Full_Stack_JavaScript/projects/restaurant_page/src/index.js
+++ Full_Stack_JavaScript/projects/restaurant_page/src/index.js
@@ -1,9 +1,37 @@
 import { createSkeleton } from './website';
-// import { createHome } from './home';
-// import { createMenu } from './menu';
+import { createHome } from './home';
+import { createMenu } from './menu';
 import { createContact } from './contact';
 
 createSkeleton();
-// createHome();
-// createMenu();
-createContact();
+createHome();
+
+function clearContent(){
+    const contactContainer = document.getElementsByClassName("contactContainer")[0];
+    const intro = document.getElementsByClassName("intro")[0];
+    const contentCards = document.getElementsByClassName("contentCards")[0];
+    contactContainer.innerText = "";
+    intro.innerText = "";
+    contentCards.innerText = "";
+}
+
+const menuButton = document.getElementsByClassName("navMenu")[0];
+menuButton.addEventListener('click', () => {
+    clearContent()
+    createSkeleton()
+    createMenu()
+})
+
+const contactButton = document.getElementsByClassName("navContact")[0];
+contactButton.addEventListener('click', () => {
+    clearContent()
+    createSkeleton()
+    createContact()
+})
+
+const homeButton = document.getElementsByClassName("navHome")[0];
+homeButton.addEventListener('click', () => {
+    clearContent()
+    createSkeleton()
+    createHome()
+})
