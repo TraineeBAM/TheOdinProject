5088d0eb8b2389c073b20f5b52840b1f8cd6e3d6
brandon.trainee <brandon.trainee@gmail.com>
2024-01-19 15:00:33 +1000

    Logic to create new ToDo's works!

--- Full_Stack_JavaScript/projects/todo/src/index.js
+++ Full_Stack_JavaScript/projects/todo/src/index.js
@@ -1,9 +1,1 @@
 import { createToDoItem } from "./todo";
-import { format, compareAsc } from "date-fns"
-
-let toDo1 = createToDoItem('Service Car', 'Change oil and filter, check brakes', '30th Jan 2024', 'medium', 'Home tasks')
-
-format(new Date(2014, 1, 11), "MM/dd/yyyy");
-console.log(Date);
-
-console.log(toDo1);
--- Full_Stack_JavaScript/projects/todo/src/todo.js
+++ Full_Stack_JavaScript/projects/todo/src/todo.js
@@ -1,10 +1,74 @@
-function createToDoItem(title, description, dueDate, priority, project){
+const newToDo = document.getElementById('newToDo');
+newToDo.addEventListener('click', (e) =>
+document.getElementById('formOverlay').style.display = 'flex')
+
+const closeForm = document.getElementById('cancelFormBtn')
+closeForm.addEventListener('click', (e) =>{
+    document.getElementById('formOverlay').style.display = 'none';
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('dueDate').value = '';
+})
+
+
+const submitForm = document.getElementById('submitFormBtn')
+submitForm.addEventListener('click', (e) => {
+    const title = document.getElementById('title').value;
+    const priority = document.getElementById('priority').value;
+    const description = document.getElementById('description').value;
+    const dueDate = document.getElementById('dueDate').value;
+    // Ensure form contains info
+    if (title.trim() === '' || priority.trim() === '' || dueDate.trim() === '') {
+        alert('Please fill in all required fields (Title, Priority, Due Date).');
+        return;
+    }
+    const toDoItem = createToDoItem(title, priority, description, dueDate);
+    console.log(toDoItem)
+
+    //Creating the content card
+    const contentCardContainer = document.getElementById('contentCardContainer');
+    const contentCard = document.createElement('div');
+    contentCard.classList.add('contentCard')
+    contentCardContainer.appendChild(contentCard);
+    //creating the content card information
+    //title
+    const toDoTitle = document.createElement('div');
+    toDoTitle.classList.add('toDoTitle');
+    toDoTitle.innerText = title;
+    contentCard.appendChild(toDoTitle);
+    //priority
+    const toDoPriority = document.createElement('div');
+    toDoPriority.classList.add('toDoPriority');
+    toDoPriority.innerText = priority;
+    contentCard.appendChild(toDoPriority);
+    //description
+    const toDoText = document.createElement('div');
+    toDoText.classList.add('toDoText');
+    toDoText.innerText = description;
+    contentCard.appendChild(toDoText);
+    //due date
+    const toDoDue = document.createElement('div');
+    toDoDue.classList.add('toDoDue');
+    toDoDue.innerText = dueDate;
+    contentCard.appendChild(toDoDue);
+    //delete button
+    const 
+
+    
+    
+    // Clear the form
+    document.getElementById('formOverlay').style.display = 'none';
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('dueDate').value = '';
+})
+
+function createToDoItem(title, priority, description, dueDate){
     return {
         title : title,
+        priority : priority,
         description : description,
         dueDate : dueDate,
-        priority : priority,
-        project : project
     }
 }
 
